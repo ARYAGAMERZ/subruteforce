d268fe4f9ea64b037c29f0fdc1a981610ea945a2
Aryan <81568644+ARYAGAMERZ@users.noreply.github.com>
2023-03-26 15:34:39 +0530

    Update sub_domain_brutefoce.py

--- sub_domain_brutefoce.py
+++ sub_domain_brutefoce.py
@@ -8,24 +8,27 @@ print(''' _____           _      _____               _
  \____/\___/ \__,_|\___|\____/_|  \__,_|___/_| |_|\___|_| 
    ''')
 
-
-
-wordlist_file = input('Enter Wordlist File: ')
+wordlist_file = input('Enter Wordlist File (Compulsory): ')
 domain = input('Enter the Target domain: ')
+output_file_name = input('Enter the Output file name: ')
+
+if not output_file_name.endswith('.txt'):
+    output_file_name += '.txt'
 
 with open(wordlist_file, 'r') as wordlist:
     subdomains = wordlist.readlines()
 
 resolver = dns.resolver.Resolver()
-resolver.nameservers = ['8.8.8.8'] 
-with open('output.txt', 'w') as output_file:
+resolver.nameservers = ['9.9.9.9'] 
+
+with open(output_file_name, 'w') as output_file:
     for subdomain in subdomains:
         subdomain = subdomain.strip()
         hostname = subdomain + '.' + domain
         try:
             answers = resolver.query(hostname, 'A')
             for rdata in answers:
-                output = f'{hostname} {rdata.address}'
+                output = f'https://{hostname}'
                 print(output)
                 output_file.write(output + '\n')
                 if len(subdomains) >= 200:
